ccd4f9bd81fbb169927bf6b6c1e5670075471c8d
LujBalzanti <Lujrbalzanti@hotmail.com>
2020-08-14 04:10:49 +0100

    Created Object Model

--- auctions/models.py
+++ auctions/models.py
@@ -2,5 +2,32 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Category(models.Model):
+    tag = models.CharField(max_length=35)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000, default="")
+    price = models.FloatField(default=0)
+    active = models.BooleanField(default=True)
+    photo = models.URLField(null=True, default=None)
+    categories = models.ManyToManyField(Category, related_name="listings")
+
 class User(AbstractUser):
+    watchlist = models.ManyToManyField(Listing)
     pass
+
+class Bid(models.Model):
+    amount = models.FloatField()
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    listingId = models.ForeignKey(Listing, related_name="bids", on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    listingId = models.ForeignKey(Listing, related_name="comments", on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    content = models.TextField(max_length=280)
+
+    def get_deleted_user():
+        return User.objects.get(username="deleted")
+    
+    userID = models.ForeignKey(User, on_delete=models.SET(get_deleted_user))
